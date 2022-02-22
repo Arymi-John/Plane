c4e4e75a280b5118ff5fb70b3ab97c2d768ca8ec
Arymi <91166012+dsdscdv@users.noreply.github.com>
2022-02-22 11:51:36 +0800

    001

--- Main/exercise.py
+++ Main/exercise.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
 
-pygame.init()
-pygame.display.init()
-screen = pygame.display.set_mode(size=(600, 800))
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.init()
+    screen = pygame.display.set_mode(size=(600, 800))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        bmg = pygame.image.load('../IMages/背景.png')
+        screen.blit(bmg, (0, 0))
 
-    pygame.display.flip()
+        pygame.display.flip()
